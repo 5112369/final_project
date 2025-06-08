d7d725b461df9eb409a5d73bad37ca0b0945b78c
5112369 <5112369@sjtu.edu.cn>
2025-06-08 23:13:57 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name='strobealign',
     version='0.1.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    author='liu zhengtao',
+    author_email='5112369.@sjtu.edu.cn',
     description='A strobe alignment algorithm for bioinformatics',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/strobealign-project',
+    url='https://github.com/5112369/final_project',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
